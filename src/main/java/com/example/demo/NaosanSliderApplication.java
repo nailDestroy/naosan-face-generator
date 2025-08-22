5131510671257be3139d31366b07b6fce8483a7d
GitHub <noreply@github.com>
2025-08-22 17:10:47 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/NaosanSliderApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaosanSliderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaosanSliderApplication.class, args);
+	}
+
+}
